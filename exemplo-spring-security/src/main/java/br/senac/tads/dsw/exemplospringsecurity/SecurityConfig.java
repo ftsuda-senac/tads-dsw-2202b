51b49fcea404d9b194d9a175f252280f09c29dd6
ftsuda-senac <ftsuda.senac@gmail.com>
2022-10-31 22:05:59 -0300

    Configuração SecurityConfig

--- exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
+++ exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.senac.tads.dsw.exemplospringsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +35,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return plainPasswordEncoder();
+    }
+    
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+       // @formatter:off
+        http.headers()
+                .cacheControl().disable() // Desabilita controle de cache
+                .frameOptions().sameOrigin() // evita problema ao abrir console do H2
+            .and().csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/favicon/**", "/webjars/**", "/css/**", "/js/**",
+                            "/font/**", "/", "/index.html", "/h2-console/**").permitAll()
+                    .antMatchers("/sessao/**").permitAll()
+                    .antMatchers("/protegido/peao").hasAuthority("ROLE_PEAO") // OU hasRole("PEAO")
+                    .antMatchers("/protegido/fodon").hasAuthority("ROLE_FODON")
+                    .antMatchers("/protegido/god").hasAuthority("ROLE_GOD")
+                    .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                    .loginPage("/login") // URL de acesso configurado no LoginController
+                    .usernameParameter("username")
+                    .passwordParameter("senha")
+                    .defaultSuccessUrl("/home")
+                    .failureUrl("/erro/login")
+                    .permitAll()
+            .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login?logout")
+                    .invalidateHttpSession(true)
+            .and()
+                .exceptionHandling()
+                    .accessDeniedPage("/erro/403");
+                    
+        // @formatter:on
     }
 
 }
--- exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/controller/LoginController.java
+++ exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/controller/LoginController.java
@@ -2,12 +2,15 @@ package br.senac.tads.dsw.exemplospringsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String mostrarFormLogin() {
+    public String mostrarFormLogin(
+            @RequestParam(value = "logout", required = false) String logoutParam
+    ) {
         return "login";
     }
 
