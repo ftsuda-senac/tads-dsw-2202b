3cc8a14df96cc0c9527f672b9d37ebebbe1c1073
ftsuda-senac <ftsuda.senac@gmail.com>
2022-10-31 22:44:49 -0300

    Uso do bcrypt

--- exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
+++ exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return plainPasswordEncoder();
+        return bcryptPasswordEncoder();
     }
     
     @Override
