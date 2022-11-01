d135a0fe826575cfa4bd0cdd9a7df4503ebd5b89
ftsuda-senac <ftsuda.senac@gmail.com>
2022-11-01 15:27:55 -0300

    Configuração do Spring Security pós versão 5.7/Boot 2.7

--- exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
+++ exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/SecurityConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+// Configuração do Spring Security pós versão 5.7/Boot 2.7
+// https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     public static PasswordEncoder plainPasswordEncoder() {
         return new PasswordEncoder() {
@@ -39,39 +39,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return bcryptPasswordEncoder();
     }
     
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-       // @formatter:off
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // @formatter:off
         http.headers()
                 .cacheControl().disable() // Desabilita controle de cache
                 .frameOptions().sameOrigin() // evita problema ao abrir console do H2
             .and().csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/favicon/**", "/webjars/**", "/css/**", "/js/**",
-                            "/font/**", "/", "/index.html", "/h2-console/**").permitAll()
-                    .antMatchers("/sessao/**").permitAll()
-//                    .antMatchers("/protegido/peao").hasAuthority("ROLE_PEAO") // OU hasRole("PEAO")
-//                    .antMatchers("/protegido/fodon").hasAuthority("ROLE_FODON")
-//                    .antMatchers("/protegido/god").hasAuthority("ROLE_GOD")
-                    .anyRequest().authenticated()
+                .antMatchers("/favicon/**", "/webjars/**", "/css/**", "/js/**",
+                        "/font/**", "/", "/index.html", "/h2-console/**").permitAll()
+                .antMatchers("/sessao/**").permitAll()
+//                .antMatchers("/protegido/peao").hasAuthority("ROLE_PEAO") // OU hasRole("PEAO")
+//                .antMatchers("/protegido/fodon").hasAuthority("ROLE_FODON")
+//                .antMatchers("/protegido/god").hasAuthority("ROLE_GOD")
+                .anyRequest().authenticated()
             .and()
                 .formLogin()
                     .loginPage("/login") // URL de acesso configurado no LoginController
                     .usernameParameter("username")
                     .passwordParameter("senha")
                     .defaultSuccessUrl("/home")
-                    .failureUrl("/erro/login")
                     .permitAll()
             .and()
                 .logout()
                     .logoutUrl("/logout")
                     .logoutSuccessUrl("/login?logout")
                     .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
             .and()
                 .exceptionHandling()
                     .accessDeniedPage("/erro/403");
-                    
         // @formatter:on
+
+        return http.build();
     }
 
 }
