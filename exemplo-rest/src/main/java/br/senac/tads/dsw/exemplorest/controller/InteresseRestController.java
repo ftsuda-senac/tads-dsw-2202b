f9d84f77027c3e712e5e5b76f2e5fcd32a450169
ftsuda-senac <ftsuda.senac@gmail.com>
2022-11-07 21:36:31 -0300

    InteresseRestController

---
+++ exemplo-rest/src/main/java/br/senac/tads/dsw/exemplorest/controller/InteresseRestController.java
@@ -0,0 +1,23 @@
+package br.senac.tads.dsw.exemplorest.controller;
+
+import br.senac.tads.dsw.exemplorest.dominio.Interesse;
+import br.senac.tads.dsw.exemplorest.dominio.InteresseRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/interesses")
+public class InteresseRestController {
+    
+    @Autowired
+    private InteresseRepository repository;
+    
+    @GetMapping
+    public List<Interesse> findAll() {
+        return repository.findAll();
+    }
+    
+}
